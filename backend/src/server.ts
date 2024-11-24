d6ca9eaca7ca1a0260d9df7aa9aa3a7a764b21b8
Dharyl Carry S. Almora <dharylovey@gmail.com>
2024-11-24 22:50:41 +0800

    ✅ Initialize Express server

---
+++ backend/src/server.ts
@@ -0,0 +1,18 @@
+import express, { Express } from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const app: Express = express()
+const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Express + TypeScript Server',
+    version: '1.0.0',
+    Healthy: true
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`)
+})
