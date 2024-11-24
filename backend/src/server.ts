c6974b401efb95a6028968789f73e02a03d257f3
Dharyl Carry S. Almora <dharylovey@gmail.com>
2024-11-24 22:56:20 +0800

    ✅Install more deps

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,15 @@
-import express, { Express } from 'express'
-import dotenv from 'dotenv'
+import express, { Express } from "express"
+import dotenv from "dotenv"
 
 dotenv.config()
 const app: Express = express()
 const PORT = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.status(200).json({
-    message: 'Express + TypeScript Server',
-    version: '1.0.0',
-    Healthy: true
+    message: "Express + TypeScript Server",
+    version: "1.0.0",
+    health: true
   })
 })
 
