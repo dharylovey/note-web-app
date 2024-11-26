658c6f57d1c7481b98afbbd4ad08b33974245dd5
Dharyl Carry S. Almora <dharylovey@gmail.com>
2024-11-26 21:41:40 +0800

    ✨Update requestLogger

--- backend/src/middleware/requestLogger.ts
+++ backend/src/middleware/requestLogger.ts
@@ -2,7 +2,7 @@ import { RequestHandler } from "express";
 
 export const requestLogger: RequestHandler = (req, res, next) => {
   try {
-    console.log(req.body, req.headers, req.params, req.query);
+    console.log(req.headers);
     next();
   } catch (error) {
     next(error);
