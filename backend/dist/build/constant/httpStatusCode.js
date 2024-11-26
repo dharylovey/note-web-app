33bc21798fdfd53f3e0623a1df487a8b08f982ce
Dharyl Carry S. Almora <dharylovey@gmail.com>
2024-11-26 21:11:12 +0800

    remove dist

--- backend/dist/build/constant/httpStatusCode.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SERVER_ERROR = exports.NOT_FOUND = exports.FORBIDDEN = exports.UNAUTHORIZED = exports.BAD_REQUEST = exports.OK = void 0;
-exports.OK = 200;
-exports.BAD_REQUEST = 400;
-exports.UNAUTHORIZED = 401;
-exports.FORBIDDEN = 403;
-exports.NOT_FOUND = 404;
-exports.SERVER_ERROR = 500;
--- backend/prisma/migrations/20241125141938_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "isVerified" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "lastLogin" TIMESTAMP(3),
-ADD COLUMN     "resetPasswordExpires" TIMESTAMP(3),
-ADD COLUMN     "resetPasswordToken" TEXT,
-ADD COLUMN     "verificationCode" TEXT,
-ADD COLUMN     "verificationCodeExpires" TIMESTAMP(3);
