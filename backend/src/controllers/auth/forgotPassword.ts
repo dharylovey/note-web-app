79156f71627de5051718f3480c5342eb388f25f8
Dharyl Carry S. Almora <dharylovey@gmail.com>
2024-11-26 22:06:21 +0800

    add comments in forgot password

--- backend/src/controllers/auth/forgotPassword.ts
+++ backend/src/controllers/auth/forgotPassword.ts
@@ -8,18 +8,26 @@ import { RequestHandler } from "express";
 
 export const forgotPassword: RequestHandler = catchErrors(async (req, res) => {
   const data: ForgotPasswordSchema = req.body;
+  // Validate the request body
   const validatedData = forgotPasswordSchema.safeParse(data);
 
-  if (!validatedData.success) return res.status(BAD_REQUEST).json({ success: false, message: ErrorCode.InvalidEmailOrPassword });
+  // If the request body is not valid
+  if (!validatedData.success)
+    return res.status(BAD_REQUEST).json({ success: false, message: ErrorCode.InvalidEmailOrPassword });
 
   const { email } = validatedData.data;
+
+  // Check if the user exists
   const user = await getUserByEmail(email);
 
+  // If the user does not exist
   if (!user) return res.status(BAD_REQUEST).json({ success: false, message: ErrorCode.UserNotFound });
 
+  // Generate the reset token and reset token expires
   const resetToken = await generateCryptoToken();
   const resetTokenExpires = await resetPasswordTokenExpires();
 
+  // Update the user
   const updatedUser = await updateEmailPassword(user.id, resetToken, resetTokenExpires);
 
   return res.status(OK).json({ success: true, message: SuccessCode.EmailSent, data: updatedUser });
