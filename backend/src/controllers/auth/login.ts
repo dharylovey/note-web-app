709d08ef48a3282138e6532de53e5402a61b1404
Dharyl Carry S. Almora <dharylovey@gmail.com>
2024-11-26 21:57:41 +0800

    add comments in login controller

--- backend/src/controllers/auth/login.ts
+++ backend/src/controllers/auth/login.ts
@@ -9,34 +9,43 @@ import { RequestHandler } from "express";
 
 export const login: RequestHandler = catchErrors(async (req, res) => {
   const data: UserSchema = req.body;
+  // Validate the request body
   const validatedData = loginSchema.safeParse(data);
 
+  // If the request body is not valid
   if (!validatedData.success)
     return res
       .status(BAD_REQUEST)
       .json({ success: false, message: ErrorCode.InvalidEmailOrPassword });
 
+  // Destructure the validated data
   const { email, password } = validatedData.data;
 
+  // Check if the user already exists
   const userExist = await getUserByEmail(email);
 
+  // If the user already exists
   if (!userExist || !userExist.password || !userExist.email)
     return res
       .status(BAD_REQUEST)
       .json({ success: false, message: ErrorCode.InvalidEmailOrPassword });
 
+  // If the user is not verified
   if (!userExist.isVerified)
     return res
       .status(BAD_REQUEST)
       .json({ success: false, message: ErrorCode.VerifyEmail });
 
+  // Compare the password
   const user = await comparePassword(password, userExist.password);
 
+  // If the password is not correct
   if (!user)
     return res
       .status(BAD_REQUEST)
       .json({ success: false, message: ErrorCode.InvalidEmailOrPassword });
 
+  // Generate the token and set the cookie
   await generateTokenSetCookie(res, userExist.id);
 
   const newUser = {
@@ -45,6 +54,7 @@ export const login: RequestHandler = catchErrors(async (req, res) => {
     isVerified: userExist.isVerified
   };
 
+  // Return the user
   return res
     .status(OK)
     .json({ success: true, message: SuccessCode.LoginSuccess, data: newUser });
