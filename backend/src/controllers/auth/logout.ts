d232274230e5862bea46b3ef35893cfd7218b488
Dharyl Carry S. Almora <dharylovey@gmail.com>
2024-11-26 21:46:33 +0800

    ✅Add logout controller

--- backend/src/controllers/auth/logout.ts
+++ backend/src/controllers/auth/logout.ts
@@ -2,7 +2,10 @@ import catchErrors from "@/utils/catchErrors";
 import { RequestHandler } from "express";
 
 export const logout: RequestHandler = catchErrors(async (req, res) => {
+  res.clearCookie("note_web_app_token");
+
   res.status(200).json({
+    success: true,
     message: "Logout Successfully"
   });
 });
--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,12 +1,14 @@
-import { forgotPassword } from "@/controllers/auth/forgotPassword"
-import { login } from "@/controllers/auth/login"
-import { register } from "@/controllers/auth/register"
-import express from "express"
+import { forgotPassword } from "@/controllers/auth/forgotPassword";
+import { login } from "@/controllers/auth/login";
+import { logout } from "@/controllers/auth/logout";
+import { register } from "@/controllers/auth/register";
+import express from "express";
 
-const authRoutes = express.Router()
+const authRoutes = express.Router();
 
-authRoutes.post("/login", login)
-authRoutes.post("/register", register)
-authRoutes.post("/forgot-password", forgotPassword)
+authRoutes.post("/login", login);
+authRoutes.post("/register", register);
+authRoutes.post("/forgot-password", forgotPassword);
+authRoutes.post("/logout", logout);
 
-export default authRoutes
+export default authRoutes;
